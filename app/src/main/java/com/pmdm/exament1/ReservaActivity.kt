6e2d857e44dff87ab0f2997ef5c22e2f55fe30ee
Iria <irialcban@alu.edu.gva.es>
2023-11-17 12:45:31 +0100

    Initial commit

---
+++ app/src/main/java/com/pmdm/exament1/ReservaActivity.kt
@@ -0,0 +1,11 @@
+package com.pmdm.exament1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReservaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserva)
+    }
+}
